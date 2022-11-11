39cbf7e746a95995ffeddac9cd3ee4fd08f6769e
JRedxs <vandevelde.e@yahoo.com>
2022-11-11 16:17:22 +0100

    EnzoHorloge

---
+++ Horloge.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import tkinter as tk
+import datetime
+import locale
+locale.setlocale(locale.LC_TIME, '')
+# myDatetime = datetime.datetime.now()
+# myString = myDatetime.strftime('%Y-%m-%d %H:%M:%S')
+# print(myString)
+
+Fenetre = tk.Tk()
+Fenetre.geometry("300x500")
+Fenetre.title("Horaire des pays")
+Fenetre.configure(bg="black")
+Fenetre.resizable(width=False, height=False)
+
+
+date = datetime.date.today()
+day = datetime.date.today()
+day1 = day.strftime('%A')
+
+
+labelday = Label(Fenetre, text=day1.upper(), background="Black", fg='White')
+labelday.place(relx=0.1, rely=0.02)
+
+labeldatejour = Label(Fenetre, text=date, background="Black", fg='White')
+labeldatejour.place(relx=0.1, rely=0.06)
+
+
+Fenetre.mainloop()
