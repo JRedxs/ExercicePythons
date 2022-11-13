46920f476ba42ce785adba8b00774a5b004dc3d2
JRedxs <vandevelde.e@yahoo.com>
2022-11-13 20:51:25 +0100

    EnzoDetection

--- DetectionLangue.py
+++ DetectionLangue.py
@@ -3,25 +3,24 @@
 #                   VANDEVELDE ENZO                         #
 #                      12-11-2022                           #
 #                                                           #
-#                                                           #
-#                                                           #
 #############################################################
 from tkinter import *
 import tkinter as tk
 
 
-def CreationFenetre():
-    Fenetre = tk.Tk()
-    Fenetre.geometry("800x400")
-    Fenetre.title("Horaire des pays")
-    Fenetre.configure(bg="grey")
-    Fenetre.resizable(width=False, height=False)
+Fenetre = tk.Tk()
+Fenetre.geometry("900x500")
+Fenetre.title("Horaire des pays")
+Fenetre.configure(bg="grey")
+Fenetre.resizable(width=False, height=False)
 
 
 def TexteInput():
-    InsertionText = Label(CreationFenetre, text="Texte : ", fg='White')
-    InsertionText.place(relx=0.2, rely=0.05)
+    InsertionText = Label(Fenetre, text="Texte : ", bg='grey', fg='White')
+    InsertionText.place(relx=0.063, rely=0.05)
+    Canvas(Fenetre, width=770, height=100, bg='White',
+           borderwidth=2, relief='solid').pack(padx=0.3, pady=70)
 
 
-CreationFenetre()
+TexteInput()
 mainloop()
